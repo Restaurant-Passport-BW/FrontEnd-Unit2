637a3edbe9de7281eb3dd47d0d71cf05cf447c9c
LezaJackson <53628558+LezaJackson@users.noreply.github.com>
2019-10-23 09:58:00 -0700

    cleaned up code

--- restaurant_passport/src/App.js
+++ restaurant_passport/src/App.js
@@ -2,17 +2,28 @@ import React from 'react';
 import logo from './logo.svg';
 import Signup from './Components/Signup';
 import Login from './Components/login'
-import {Route} from "react-router-dom"
+import {
+  Route
+} from "react-router-dom"
 
 import './App.css';
 
 
 function App() {
-  return (
-    <div className="App">
-     <Route path= "/Signup" component= {Signup}/>
-      <Route exact path="/" component= {Login}/>
-    </div>
+  return ( <
+    div className = "App" >
+    <
+    Route path = "/Signup"
+    component = {
+      Signup
+    }
+    /> <
+    Route exact path = "/"
+    component = {
+      Login
+    }
+    /> <
+    /div>
   );
 }
 
